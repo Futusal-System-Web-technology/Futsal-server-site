6735b4f1610e3f9f992ddf3e752ff6a2c1da2d60
gmshimon <simon.rosedale99@gmail.com>
2023-07-04 09:06:54 +0800

    post booking

--- src/modules/Futsal/futsal.controller.js
+++ src/modules/Futsal/futsal.controller.js
@@ -1,4 +1,11 @@
 const { getDb } = require("../../../utilis/dbConfig")
+// Import the 'date-fns' library (optional, for formatting the date)
+const { format } = require('date-fns');
+// Create a new Date object
+const today = new Date();
+// Format the date using 'date-fns' (optional)
+const formattedDate = format(today, 'yyyy-MM-dd');
+
 
 module.exports.postFutsal = async(req,res,next) => {
     try {
@@ -30,5 +37,28 @@ module.exports.getAllFutsal = async(req,res,next)=>{
         res.status(200).json({status:'success',data: results});
     } catch (error) {
         
+    }
+}
+
+module.exports.postBooking = async(req,res,next)=>{
+    try {
+        const db = getDb()
+
+        const data = req.body;
+        data.date = formattedDate
+   
+        const results = await db.collection('futsal-bookings').insertOne(data);
+
+        res.status(200).json({
+            status:"success",
+            message:"Booking has been created successfully",
+            data: data
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            status:"Fail",
+            message:error.message
+        })
     }
 }
--- src/modules/Futsal/futsal.router.js
+++ src/modules/Futsal/futsal.router.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { postFutsal, getAllFutsal } = require('./futsal.controller');
+const { postFutsal, getAllFutsal, postBooking } = require('./futsal.controller');
 const router = express.Router();
 
 router.route('/').get(getAllFutsal)
+router.route('/futsal-booking').post(postBooking)
 router.route('/post-futsal').post(postFutsal);
 
 module.exports = router;
