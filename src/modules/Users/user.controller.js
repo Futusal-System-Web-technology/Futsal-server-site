7e094fd7b94082539fcd01ce09e996c0326e1938
gmshimon <simon.rosedale99@gmail.com>
2023-07-30 01:26:28 +0800

    [DONE] update booking status done

--- src/modules/Users/user.controller.js
+++ src/modules/Users/user.controller.js
@@ -3,46 +3,78 @@ const { getDb } = require('../../../utilis/dbConfig')
 const bcrypt = require('bcrypt')
 const { generateToken } = require('../../../utilis/token')
 
-module.exports.getAllUsers = async (req,res,next)=>{
-    try {
-        const db = getDb();
-        const users = await db.collection("Users").find().toArray();
+module.exports.getAllUsers = async (req, res, next) => {
+  try {
+    const db = getDb()
+    const users = await db.collection('Users').find().toArray()
 
-        if (users.length == 0) {
-            return res.status(400).json({ success: 'Fail', error: "No data" });
-        }
-
-        res.status(200).json({ success: 'success', data: users });
-    } catch (error) {
-        res.status(400).json({
-            status: 'Fail',
-            message: error.message,
-          });
+    if (users.length == 0) {
+      return res.status(400).json({ success: 'Fail', error: 'No data' })
     }
+
+    res.status(200).json({ success: 'success', data: users })
+  } catch (error) {
+    res.status(400).json({
+      status: 'Fail',
+      message: error.message
+    })
+  }
 }
 
-module.exports.getMyBooking = async(req,res,next)=>{
-    try {
-        const db = getDb();
-        const {username} = req.body;
-        const bookings = await db.collection('futsal-bookings').find({username:username}).sort({"_id": -1}).toArray()
+module.exports.getMyBooking = async (req, res, next) => {
+  try {
+    const db = getDb()
+    const { username } = req.body
+    const bookings = await db
+      .collection('futsal-bookings')
+      .find({ username: username })
+      .sort({ _id: -1 })
+      .toArray()
 
-        res.status(200).send({
-            status: 'success',
-            data:bookings
-        });
-    } catch (error) {
-        res.status(400).json({
-            status:"Fail",
-            message: error.message
-        })
-    }
+    res.status(200).send({
+      status: 'success',
+      data: bookings
+    })
+  } catch (error) {
+    res.status(400).json({
+      status: 'Fail',
+      message: error.message
+    })
+  }
 }
 
+module.exports.updateBooking = async (req, res, next) => {
+  try {
+    const db = getDb()
+    const { id, status } = req.body
+    console.log(id, status)
+    // const bookings = await db
+    //   .collection('futsal-bookings')
+    //   .find({ _id: ObjectId(id) })
+    //   .toArray()
+
+    // Update the booking status
+    const result = await db
+      .collection('futsal-bookings')
+      .updateOne({ _id: ObjectId(id) }, { $set: { 'futsal.status': status } })
+    console.log(
+      `Successfully updated ${result.modifiedCount} futsal object(s).`
+    )
+    res.status(200).send({
+      status: 'success',
+      data: result
+    })
+  } catch (e) {
+    res.status(400).json({
+      status: 'Fail',
+      message: e.message
+    })
+  }
+}
 
 module.exports.postUser = async (req, res, next) => {
   try {
-    const { email,username, password } = req.body
+    const { email, username, password } = req.body
     let newPassword
     const db = getDb()
 
@@ -58,7 +90,7 @@ module.exports.postUser = async (req, res, next) => {
 
     setTimeout(async () => {
       const newUser = {
-        email:email,
+        email: email,
         userName: username,
         role: 'user',
         password: newPassword
@@ -78,55 +110,54 @@ module.exports.postUser = async (req, res, next) => {
   }
 }
 
+module.exports.loginUser = async (req, res, next) => {
+  try {
+    const { username, password } = req.body
 
-module.exports.loginUser = async(req,res,next)=>{
-    try {
-        const{username,password} = req.body;
-
-        if(!username||!password){
-            return res.status(401).json({
-                status:"Fail",
-                message:"Please provide credentials"
-            })
-        }
-        const db = getDb()
-        const user = await db.collection('Users').findOne({userName:username});
-        if(!user){
-            return res.status(401).json({
-                status:"Fail",
-                message:"No user with this username"
-            })
-        }
-
-        const isPasswordValid = bcrypt.compareSync(password,user.password);
-
-        if(!isPasswordValid){
-            return res.status(403).json({
-                status:"Fail",
-                message:"Password is not correct"
-            })
-        }
-
-        if(user.status == 0){
-            return res.status(403).json({
-                status:"Fail",
-                message:"Your account is not active"
-            })
-        }
-        const token = generateToken(user);
-        const {password:pwd,...others} = user
-        res.status(200).json({
-            status:"success",
-            message:"successfully singed in",
-            data:{
-                others,
-                token
-            }
-        })
-    } catch (error) {
-        res.status(404).json({
-            status:"Fail",
-            message:error.message,
-        }) 
+    if (!username || !password) {
+      return res.status(401).json({
+        status: 'Fail',
+        message: 'Please provide credentials'
+      })
     }
+    const db = getDb()
+    const user = await db.collection('Users').findOne({ userName: username })
+    if (!user) {
+      return res.status(401).json({
+        status: 'Fail',
+        message: 'No user with this username'
+      })
+    }
+
+    const isPasswordValid = bcrypt.compareSync(password, user.password)
+
+    if (!isPasswordValid) {
+      return res.status(403).json({
+        status: 'Fail',
+        message: 'Password is not correct'
+      })
+    }
+
+    if (user.status == 0) {
+      return res.status(403).json({
+        status: 'Fail',
+        message: 'Your account is not active'
+      })
+    }
+    const token = generateToken(user)
+    const { password: pwd, ...others } = user
+    res.status(200).json({
+      status: 'success',
+      message: 'successfully singed in',
+      data: {
+        others,
+        token
+      }
+    })
+  } catch (error) {
+    res.status(404).json({
+      status: 'Fail',
+      message: error.message
+    })
+  }
 }
--- src/modules/Users/user.router.js
+++ src/modules/Users/user.router.js
@@ -1,10 +1,17 @@
-const express = require('express');
-const { postUser, loginUser, getAllUsers, getMyBooking } = require('./user.controller');
-const router = express.Router();
+const express = require('express')
+const {
+  postUser,
+  loginUser,
+  getAllUsers,
+  getMyBooking,
+  updateBooking
+} = require('./user.controller')
+const router = express.Router()
 
 router.route('/').get(getAllUsers)
 router.route('/my-booking').post(getMyBooking)
 router.route('/register').post(postUser)
 router.route('/login').post(loginUser)
+router.route('/update-booking').put(updateBooking)
 
-module.exports = router;
+module.exports = router
